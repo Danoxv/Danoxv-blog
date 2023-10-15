40f56942339f631296b9f74b2373ffcaac420d5f
danoxv <denisiv.andreevi@mail.ru>
2023-10-15 15:27:24 +0300

    Update config.ts

--- resources/js/config.ts
+++ resources/js/config.ts
@@ -3,7 +3,7 @@
  */
 
 // App name
-export const APP_NAME = import.meta.env.VITE_APP_NAME || 'Sijeko';
+export const APP_NAME = import.meta.env.VITE_APP_NAME || 'Danoxv';
 // App version in the form of: <major>.<minor>.<build>+<SHA>
 export const APP_VERSION = import.meta.env.VITE_APP_VERSION || '0.1.666+abcdef7';
 
